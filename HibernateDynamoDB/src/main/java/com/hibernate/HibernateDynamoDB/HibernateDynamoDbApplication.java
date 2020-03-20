88cf1c3b5047d25dde5bb1a0aee499b43d018523
absatish <47278791+absatish@users.noreply.github.com>
2020-03-20 19:22:11 +0530

    Add files via upload

---
+++ HibernateDynamoDB/src/main/java/com/hibernate/HibernateDynamoDB/HibernateDynamoDbApplication.java
@@ -0,0 +1,12 @@
+package com.hibernate.HibernateDynamoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateDynamoDbApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateDynamoDbApplication.class, args);
+	}	
+}
